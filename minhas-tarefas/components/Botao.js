dc880d14d66a48696d3152b68ddb78b231751535
unknown <2312082003@academico.iesb.br>
2026-03-04 21:53:45 -0300

    first commit

---
+++ minhas-tarefas/components/Botao.js
@@ -0,0 +1,8 @@
+// import React, {Component} from "react";
+// import { Button, Text } from "react-native";
+
+// class Botao extends React.Component {
+//     render() {
+//         return <Button>Click</Button>
+//     }
+// }
